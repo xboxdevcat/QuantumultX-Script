22f3769d625d15c0604cd97d9ef2f860ec859471
xboxdevcat <constantine_yr@outlook.com>
2022-02-14 21:23:25 +0800

    Update bilibili_manga.js
    删除Tabbar中间的广告图片

--- bilibili_manga.js
+++ bilibili_manga.js
@@ -1,6 +1,7 @@
 let obj = JSON.parse($response.body)
 delete obj.data.flash
 delete obj.data.operate
+delete obj.data.activity_tab
 obj.data.teenager_frequency = 0
 body=JSON.stringify(obj)
 $done({body})
